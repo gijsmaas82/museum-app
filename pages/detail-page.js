b730baf6a8547e6fe2a7748c518ebee3746e6091
gijsmaas82 <gijsmaas82@outlook.com>
2019-08-07 11:27:57 +0200

    display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -11,13 +11,17 @@ function submitComment() {
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
     //change the name and message of the user inside the html of the page
-    h3.innerHTML = `${name}`
-    p.innerHTML = `${msg}`
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = `"${msg}"`
     //creating the space on the page by appending the html
     comment.classList.add('comment')
     comment.appendChild(h3)
     comment.appendChild(p)
-    console.log(comment)
+    //getting the commentsection of the page and appending the new comment as a child
+    let commentSection = document.getElementById('comments')
+    commentSection.appendChild(comment)
+    inputField.value = null
+    inputMessage.value = null
 }
 
 
