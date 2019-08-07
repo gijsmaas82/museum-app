53fe8e2d846eccabe3b717971d5586c42f4b326b
gijsmaas82 <gijsmaas82@outlook.com>
2019-08-07 11:12:50 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,23 @@
+// Event handler 
 function submitComment() {
-    console.log("hello")
+    // getting the name from the user
+    let inputField = document.getElementById('name')
+    let name = inputField.value
+    // getting the message from the user
+    let inputMessage = document.getElementById('msg')
+    let msg = inputMessage.value
+    // creating elements to show name and message from user on the page
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+    //change the name and message of the user inside the html of the page
+    h3.innerHTML = `${name}`
+    p.innerHTML = `${msg}`
+    //creating the space on the page by appending the html
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+    console.log(comment)
 }
+
+
